fe887f1ed4bb8242b172a507eb24786fa1955005
jiangwx <christophebigbear@163.com>
2018-12-06 02:40:17 +0800

    add GlobalAvgPool

--- test.py
+++ test.py
@@ -40,6 +40,19 @@ args = parser.parse_args()
 if not args.model:
     print("=> no model found")
 
+class GlobalAvgPool2d(nn.Module):
+    def __init__(self):
+        super(GlobalAvgPool2d, self).__init__()
+
+    def forward(self, x):
+        N = x.data.size(0)
+        C = x.data.size(1)
+        H = x.data.size(2)
+        W = x.data.size(3)
+        x = F.avg_pool2d(x, (H, W))
+        x = x.view(N, C)
+        return x
+
 def test(model, data_loader, loss_func):
     model.eval()
     correct = 0
